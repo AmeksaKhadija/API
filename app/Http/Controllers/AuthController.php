8829a18e003b436ea916e77c08495d1e058a3760
khadija <ameksakhadija026@gmail.com>
2024-03-25 16:04:50 +0000

    authentification

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        try {
+            $user = new User();
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            $user->password = bcrypt($request->input('password'));
+            $user->role_id = 1 ;
+            $user->save();
+
+            return response()->json(['message' => 'Utilisateur enregistré avec succès'], 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Une erreur est survenue lors de l\'enregistrement de l\'utilisateur: ' . $e->getMessage()], 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Identifiants incorrects'], 401);
+        }
+
+        return response()->json(['message' => 'Connexion réussie'], 200);
+    }
+
+}
